2821dc9d0c85378f1d866a04cb94c3d28193fdf0
sdavisde <sdavisde@gmail.com>
2025-12-12 23:04:54 -0600

    feat: add new lint rules to prevent returning Error from server action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import prettier from 'eslint-config-prettier/flat'
+import tsParser from '@typescript-eslint/parser'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,21 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
+  {
+    files: ['**/actions/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: "TSTypeReference[typeName.name='Result'] > TSTypeParameterInstantiation > TSTypeReference[typeName.name='Error']",
+          message: "Do not use Result<Error, ...> in Server Actions. Error objects are not serializable to the client. Use Result<string, ...> instead."
+        }
+      ]
+    }
+  }
 ])
 
 export default eslintConfig
