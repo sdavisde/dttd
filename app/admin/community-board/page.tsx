895500aae5eb1eef5e31e6b134742dfc7ad931cf
nicholas-fierro <naf5301@gmail.com>
2026-02-02 21:01:52 -0600

    database.types.ts

--- app/admin/community-board/page.tsx
+++ app/admin/community-board/page.tsx
@@ -1,5 +1,4 @@
 import { AdminBreadcrumbs } from '@/components/admin/breadcrumbs'
-import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import {
